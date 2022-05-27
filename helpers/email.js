1f7120f252ceb99e971864881ed06296715b98df
ArechuaDaniel <daniel.ar98@hotmail.com>
2022-05-26 23:42:19 -0500

    Primer Commit

---
+++ helpers/email.js
@@ -0,0 +1,60 @@
+import nodemailer from 'nodemailer';
+ 
+export const emailRegistro = async (datos) => {
+    
+    const {email, nombre, token } = datos;
+ 
+    
+    const transport = nodemailer.createTransport({
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS
+      }
+    });
+ 
+      // Información del email
+ 
+      const info = await transport.sendMail({
+          from: '"Uptask - Administrador de Proyectos" <cuentas@uptask.com',
+          to: email,
+          subject: "Uptask - Comprueba tu cuenta",
+          text: "Conprueba tu cuenta en UPTask",
+          html:`<p>Hola: ${nombre}. Comprueba tu cuenta </p>
+          <p>Tu cuenta ya está casi disponible, sólo debes confirmarla en el siguiente enlace</p>
+          <a href="${process.env.FRONTEND_URL}/confirmar/${token}">Comprobar cuenta</a>
+          <p>Si no creaste esta cuenta, por favor ignora este email</p>
+          `
+      })
+ 
+};
+
+export const emailOlvidePassword = async (datos) => {
+    
+  const {email, nombre, token } = datos;
+
+  const transport = nodemailer.createTransport({
+    host: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT,
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS
+    }
+  });
+
+    // Información del email
+
+    const info = await transport.sendMail({
+        from: '"Uptask - Administrador de Proyectos" <cuentas@uptask.com',
+        to: email,
+        subject: "Uptask - Restablece tu Password",
+        text: "Conprueba tu cuenta en UPTask",
+        html:`<p>Hola: ${nombre}. Has solicitado reestablecer tu password </p>
+        <p>Sigue el siguiente enlace para generar un nuevo password</p>
+        <a href="${process.env.FRONTEND_URL}/olvide-password/${token}">Reestablecer Password</a>
+        <p>Si no solicitaste este email, por favor ignora este email</p>
+        `
+    })
+
+};
